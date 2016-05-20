4a0b2ceafc266b19bcad6472d51d9f5eb40fb21c
Rondel21 <ronald.deleon800@gmail.com>
2016-05-20 10:30:08 -0700

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,5 +3,8 @@ This is empty on purpose! Your code to build the resume will go here.
  */
  var firstName = "Ronald";
  var age = 30;
- console.log(Ronald);
+ console.log(firstName);
+ 
+ var awesomeThoughts = "I am Ronald and I am AWESOME!";
+ console.log(awesomeThoughts);
  
